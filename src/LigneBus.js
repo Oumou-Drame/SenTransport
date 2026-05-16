f58233c6d6ee20d52fb914b990917e3ede04528a
Oumou Drame <bintdrame02@gmail.com>
2026-05-16 22:11:55 +0000

    modification au niveau du fichier LigneBus.js

--- src/LigneBus.js
+++ src/LigneBus.js
@@ -1,19 +1,21 @@
-import './LigneBus.css';
+import "./LigneBus.css";
 
 function LigneBus({ numero, depart, arrivee, arrets,couleur }) {
   return (
     <div className="ligne-bus">
-      <div className="ligne-numero" style = {{backgroundColor : couleur}}>
-         {numero}
+      <div className="ligne-numero">
+        {numero}
       </div>
+
       <div className="ligne-info">
-        <span className='ligne-trajet'>
+        <span className="ligne-trajet">
           {depart} &rarr; {arrivee}
         </span>
+
+        <span className="ligne-arrets">
+          {arrets} arrêts
+        </span>
       </div>
-      <span className="ligne-arrets">
-        {arrets} arrêts
-      </span>
     </div>
   );
 }
