c5303862d86b83b0ede9187b0140a402e1c87c53
Oumou Drame <bintdrame02@gmail.com>
2026-05-29 13:05:31 +0000

    exercices Lab4

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -19,6 +19,46 @@ def accueil():
 @app.route("/lignes")
 def get_lignes():
     return jsonify(lignes)
+
+@app.route("/listeArrets")
+def get_listeArrets():
+    tous_les_arrets = []
+    for ligne in lignes:
+        tous_les_arrets.extend(ligne["listeArrets"])
+    arrets_uniques = list(set(tous_les_arrets))
+    return jsonify({
+        "arrets" : arrets_uniques})    
+
+
+@app.route("/stats")
+def get_stats():
+    nombre_de_lignes = len(lignes)
+    tous_les_arrets = 0
+    arrets_max = 0
+    numero_ligne = ""
+    for ligne in lignes : 
+        tous_les_arrets +=ligne["arrets"]
+    for ligne in lignes :
+        if arrets_max < ligne["arrets"] :
+            arrets_max = ligne["arrets"]
+            numero_ligne = ligne["numero"]
+    return jsonify({
+        "Nombre de lignes" : nombre_de_lignes,
+        "Nombre d'arrets total" :tous_les_arrets,
+        "Numero ligne ayant le plus grand nombre d'arrets" : numero_ligne,
+        "Nombre d'arrets de la ligne ayant le plus grand nombre d'arrets" : arrets_max
+    })
+@app.route("/lignes/recherche")
+def get_ligne_recherchee():
+    ligne_recherchee = request.args.get("q","")
+    lignesFiltrees = []
+    for ligne in lignes :
+        if ligne_recherchee in ligne["depart"] or ligne_recherchee in ligne["arrivee"] :
+            lignesFiltrees.append(ligne)
+    return jsonify({
+        "ligne" : lignesFiltrees
+    })        
+
 
 @app.route("/lignes/<int:ligne_id>")
 def get_ligne(ligne_id):
